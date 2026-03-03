e0ff5cc03489361b248b6805da3365fad8e68d08
mwakidenis <mwakidenice@gmail.com>
2026-03-03 17:18:59 +0300

    Update local changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -100,7 +100,7 @@ const Footer = () => {
 
       {/* Copyright Bar */}
       <div className="border-t border-gray-200">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        <div className="w-full mx-auto px-2 py-6">
           <div className="footer-bottom">
             <div className="copyright">© {new Date().getFullYear()} mwakidev. All rights reserved</div>
             <div className="payment">
